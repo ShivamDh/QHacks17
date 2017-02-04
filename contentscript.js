e61f4573cc556507de058a8c3a920eb46067bf93
Michael Maxwell <mike.ian.maxwell@gmail.com>
2017-02-04 13:21:19 -0500

    Doesn't load images on webpage before replacing them anymore

--- contentscript.js
+++ contentscript.js
@@ -1,9 +1,9 @@
-//alert ("hello");
-//document.body.style.background = 'yellow';
-
-var list = document.querySelectorAll("img");
-console.log(list.length)
-for(var i = 0; i < list.length; i++) {
-	console.log(i)
-	list[i].src = 'http://cdn-s3.si.com/s3fs-public/[current-date:custom:Y]/[current-date:custom:m]/[current-date:custom:d]/best-crying-jordan-memes.jpg'
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+	console.log("DOM fully loaded and parsed")
+	var list = document.querySelectorAll("img")
+	console.log(list.length)
+	for(var i = 0; i < list.length; i++) {
+		list[i].src = 'http://cdn-s3.si.com/s3fs-public/[current-date:custom:Y]/[current-date:custom:m]/[current-date:custom:d]/best-crying-jordan-memes.jpg'
+		//list[i].src = 'http://shimage.com/pic/' + list[i].src
+	}
+ })
