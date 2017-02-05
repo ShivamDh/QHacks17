9a51475736b521071e459b588ec0c9b53fd5360d
Michael Maxwell <mike.ian.maxwell@gmail.com>
2017-02-05 10:01:33 -0500

    min-change

--- quikpic.js
+++ quikpic.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(event) {
 	var list = document.querySelectorAll('img')
-	// TODO: get base url of website
-	var base = 
+	var host = document.location.host
 
 	for(var i = 0; i < list.length; i++) {
-		// TODO: escape the link
 		// TODO: add the root of the images domain to the link
 			// eg: img src = 'img/pic.png'
 			// =>  rootdomain + img.src
 			//     micmax.pw + img/pic.png
 
-		// replace imgur image with list[i].src
+		//  list[i].src
 		// 'http://i.imgur.com/cVjim4r.jpg'
-		var link = encodeURIComponent(base + list[i].src)
+		var link = encodeURIComponent('http://i.imgur.com/cVjim4r.jpg')
 		list[i].src = 'http://localhost:3000/' + link
 	}
  })
